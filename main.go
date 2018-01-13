12fdc6ec5ee3bbe5961224241ea83fe5016ab794
Rogier Lommers <rogier@lommers.org>
2018-01-13 09:06:28 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"io/ioutil"
+
+	humanize "github.com/dustin/go-humanize"
+	"github.com/miniflux/miniflux-go"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	targetFile string
+	username   string
+	password   string
+	hostname   string
+)
+
+func main() {
+	flag.StringVar(&targetFile, "o", "opml.xml", "output filename, f.e. /tmp/opml.xml")
+	flag.StringVar(&username, "user", "", "miniflux username")
+	flag.StringVar(&password, "pass", "", "miniflux password")
+	flag.StringVar(&hostname, "host", "http://localhost:8080", "miniflux hostname, f.e. http://localhost:8080")
+	flag.Parse()
+
+	// start backup
+	client := miniflux.NewClient(hostname, username, password)
+
+	opmlFile, err := client.Export()
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	err = ioutil.WriteFile(targetFile, opmlFile, 0644)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	logrus.Infof("backup done, %s written to file %s", humanize.Bytes(uint64(len(opmlFile))), targetFile)
+}
---
+++ makefile
@@ -0,0 +1,4 @@
+source := main.go
+
+release:
+	GOOS=linux GOARCH=amd64 go build -o miniflux-backup ${source}
